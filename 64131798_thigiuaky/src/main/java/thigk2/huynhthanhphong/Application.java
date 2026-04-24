5aceb5e293837736e86af3e80869a9d534e01d7a
phonght64cntt-clc <phong.ht.64cntt@ntu.edu.vn>
2026-04-24 14:23:17 +0700

    Lần thứ 4

--- 64131798_thigiuaky/src/main/java/thigk2/huynhthanhphong/Application.java
+++ 64131798_thigiuaky/src/main/java/thigk2/huynhthanhphong/Application.java
@@ -4,29 +4,24 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import thigk2.huynhthanhphong.models.*;
-import thigk2.huynhthanhphong.repositories.*;
+import thigk2.huynhthanhphong.models.TheLoaiTin;
+import thigk2.huynhthanhphong.repositories.TheLoaiTinRepository;
 
 @SpringBootApplication
 public class Application {
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
     @Bean
-    public CommandLineRunner initData(TheLoaiTinRepository tlRepo, TinRepository tinRepo) {
+    public CommandLineRunner initData(TheLoaiTinRepository repo) {
         return args -> {
-            if (tlRepo.count() == 0) {
-                TheLoaiTin tl1 = new TheLoaiTin(); tl1.setTenTheLoai("Thời sự");
-                tlRepo.save(tl1);
-                
-                Tin t1 = new Tin();
-                t1.setTieuDe("Tin khẩn cấp");
-                t1.setNoiDung("Nội dung tin thi giữa kỳ...");
-                t1.setTheLoaiTin(tl1);
-                tinRepo.save(t1);
-                
-                System.out.println(">>> ĐÃ TỰ ĐỘNG NẠP DỮ LIỆU THÀNH CÔNG!");
+            if (repo.count() == 0) {
+                TheLoaiTin tl1 = new TheLoaiTin();
+                tl1.setTenTheLoai("Thời sự");
+                repo.save(tl1);
+                System.out.println(">>> DATABASE CONNECTED & INITIALIZED!");
             }
         };
     }
