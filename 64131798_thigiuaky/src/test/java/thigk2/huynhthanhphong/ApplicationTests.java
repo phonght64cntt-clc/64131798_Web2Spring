049921ece18dbbc575258505590b67a2daaabe66
phonght64cntt-clc <phong.ht.64cntt@ntu.edu.vn>
2026-04-24 13:48:16 +0700

    LẦN THỨ 1

---
+++ 64131798_thigiuaky/src/test/java/thigk2/huynhthanhphong/ApplicationTests.java
@@ -0,0 +1,13 @@
+package thigk2.huynhthanhphong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
