068f6e090c7799e5bc082553dab978bb83280855
phonght64cntt-clc <phong.ht.64cntt@ntu.edu.vn>
2026-04-24 14:09:20 +0700

    LẦN THỨ 3

--- 64131798_thigiuaky/src/main/java/thigk2/huynhthanhphong/Application.java
+++ 64131798_thigiuaky/src/main/java/thigk2/huynhthanhphong/Application.java
@@ -1,13 +1,33 @@
 package thigk2.huynhthanhphong;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import thigk2.huynhthanhphong.models.*;
+import thigk2.huynhthanhphong.repositories.*;
 
 @SpringBootApplication
 public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
-
+    @Bean
+    public CommandLineRunner initData(TheLoaiTinRepository tlRepo, TinRepository tinRepo) {
+        return args -> {
+            if (tlRepo.count() == 0) {
+                TheLoaiTin tl1 = new TheLoaiTin(); tl1.setTenTheLoai("Thời sự");
+                tlRepo.save(tl1);
+                
+                Tin t1 = new Tin();
+                t1.setTieuDe("Tin khẩn cấp");
+                t1.setNoiDung("Nội dung tin thi giữa kỳ...");
+                t1.setTheLoaiTin(tl1);
+                tinRepo.save(t1);
+                
+                System.out.println(">>> ĐÃ TỰ ĐỘNG NẠP DỮ LIỆU THÀNH CÔNG!");
+            }
+        };
+    }
 }
